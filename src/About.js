336b11bd71950b8069b15bfc40855045e4db6440
PMS06 <111035168+PMS06@users.noreply.github.com>
2023-08-18 11:34:56 +0100

    first commit

--- src/About.js
+++ src/About.js
@@ -52,9 +52,12 @@ const AboutText = styled.div`
     text-align: justify;
     line-height: 2;
     color: ${(props) => props.theme.descriptionColor};
+    order: 0;
 
     @media (max-width: 1180px) {
       text-align: justify;
+      order: 1;
+      font-size: 0.8rem;
     }
 `;
 
@@ -63,20 +66,61 @@ const AboutImage = styled.img`
     max-width: 50%;
     border-radius: 10px;
     transition: all 0.3s ease;  
+    order: 1;
 
     @media (max-width: 1180px) {
+        order: 0;
         width: 90%; 
         height: 200px;  
         object-fit: contain; 
-        margin-top: 30px;
+        margin-bottom: 20px;
+        
     }
 
     @media (max-width: 768px) { 
         max-width: 100%;
         height: 200px; 
-        margin-top: 30px;
+        margin-bottom: 20px;
+    
     }
 `;
+
+const SketchedCircle = styled.span`
+  display: inline-block;
+  position: relative;
+  padding: 0em 0.65em;
+
+  
+  font-family: 'Patrick Hand', sans-serif;
+    font-weight: bold;
+
+  &::before, &::after {
+
+    content: "";
+    position: absolute;
+    width: 100%;  
+    height: 150%; 
+    border: 3px solid #FFD700;
+    border-radius: 50%;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  &::before {
+    border-top: none;
+    border-left: none;
+    transform: translate(-50%, -50%) rotate(25deg);
+  }
+
+  &::after {
+    border-bottom: none;
+    border-right: none;
+    transform: translate(-50%, -50%) rotate(-25deg);
+  }
+`;
+
+
 
 
 
@@ -92,8 +136,8 @@ const About = ({ isdarktheme }) => {
           <p>
             Hello! I'm Phone Myint San, a passionate 20-year-old full-stack developer from Myanmar. In the midst of the COVID-19 pandemic, while the world paused, my journey in the realm of technology began with HTML & CSS.
           </p>
-          <p>
-            Recently, I finished studying at Chichester College. I later transitioned to a pivotal role at an education agency. In this capacity, I've taken the lead in developing the website and agent portal. From my humble beginnings in programming during the pandemic to now shaping digital solutions, my journey underscores my dedication and evolving skill set.
+           <p>
+            Recently, I finished studying at Chichester College. I later transitioned to a pivotal role at <SketchedCircle>SafeNet</SketchedCircle>, an education agency. In this capacity, I've taken the lead in developing the website and agent portal. From my humble beginnings in programming during the pandemic to now shaping digital solutions, my journey underscores my dedication and evolving skill set.
           </p>
         </AboutText>
         <AboutImage src={aboutImage} alt="About me" />
